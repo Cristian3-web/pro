4ca25c5fe411805b6c2e8bfb12b852eb4704e722
Cristian <Cristianbue@protonmail.com>
2025-07-05 21:05:05 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -2,6 +2,17 @@
 # pylint: disable=C0116,W0613
 # This program is dedicated to the public domain under the CC0 license.
 
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
 import logging
 import os
 
@@ -19,6 +30,7 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments update and
 # context.
 def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Hi {user.mention_markdown_v2()}\!',
@@ -27,15 +39,17 @@ def start(update: Update, context: CallbackContext) -> None:
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-
+    """Echo the user message."""
     update.message.reply_text(update.message.text)
 
 
 def main() -> None:
+    """Start the bot."""
     # Create the Updater and pass it your bot's token.
     updater = Updater(os.environ.get("TOKEN", 'PUT_YOUR_TOKEN_HERE'))
 
